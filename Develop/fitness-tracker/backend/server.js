91fd87cf497b3bcc34856d45b92252f326a75877
Kamille <tramia25@gmail.com>
2021-06-30 05:05:55 -0400

    adding routes and users

---
+++ Develop/fitness-tracker/backend/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+// this the middleware this allows us to parse Json
+
+app.use(cors());
+app.use(express.json());
+
+// this connects use to the database
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true})
+
+const connection = mongoose.connection;
+connection.once('open', ()=> {
+    console.log("MongoDB database connection established succesfully");
+
+})
+
+const exerciseRouter = require('./routes/exercises');
+const userRouter = require('./routes/users');
+
+app.use('/exercises', exerciseRouter);
+app.use('/users', userRouter);
+
+// this allows for us to listen and start server 
+
+app.listen(port, () => {
+    console.log(`Server is running on Port: ${port}`);
+});
