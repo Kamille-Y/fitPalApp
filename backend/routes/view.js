58e1b052ed6d183672971a94e667013da0d5bb6c
Kamille <tramia25@gmail.com>
2021-07-30 12:44:36 -0400

    OMG I finally got this to work

--- backend/routes/view.js
+++ backend/routes/view.js
@@ -5,19 +5,19 @@ const path = require('path');
 // const { response } = require('express');
 
 router.route('/').get((req, res) => {
-  res.sendFile(path.join("../../public/exercise.html"));
+  res.sendFile(path.join(__dirname,"../../public/exercise.html"));
   workout.find()
     .then(workouts => res.json(workouts))
     .catch(err => res.status(400).json('Error: ' + err));
 });
 router.get("/stats", (req, res) => {
-  res.sendFile(path.join( "../../public/stats.html"));
+  res.sendFile(path.join(__dirname, "../../public/stats.html"));
   stats.find()
   .then(stats => res.json(stats))
   .catch(err => res.status(400).json('Error: ' + err));
 });
 router.get("/exercise", (req, res) => {
-  res.sendFile(path.join ( "../../public/exercise.html"));
+  res.sendFile(path.join (__dirname, "../../public/exercise.html"));
 });
 
 
